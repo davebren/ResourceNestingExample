3577b13b646e2d34e1c564eb5046187c46482296
Eskimo Apps <eskimoapps@gmail.com>
2014-03-15 12:12:18 -0400

    Initial commit

---
+++ TestModule/src/main/java/org/nest/resources/MainActivity.java
@@ -0,0 +1,11 @@
+package org.nest.resources;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_container);
+    }
+}
